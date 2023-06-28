8e6f7958ff5e2eaf8cea0b12d09fbc1150385504
damidaramola <damidaramola7@gmail.com>
2023-06-28 21:24:05 +0000

    conect to external database

--- canvascorner_drf_api/settings.py
+++ canvascorner_drf_api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 if os.path.exists('env.py'):
     import env
 
@@ -30,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9lzxo04!*y1%rv!!iynrcr_t06056f(x&aep5+3u-#&)-^*11%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-damidaramol-canvascorne-c86zo2ts1at.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['8000-damidaramol-canvascorne-c86zo2ts1at.ws-eu101.gitpod.io']
 
 
 # Application definition
@@ -85,13 +85,18 @@ WSGI_APPLICATION = 'canvascorner_drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+    print('connected')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
