dd036186e3e8eb561146e9bec94cdfd25e28d59c
damidaramola <damidaramola7@gmail.com>
2023-06-29 23:37:48 +0000

    create profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 # Create your models here.
+"""
+profile model class
+has a one-to-one relationship with user model
+"""
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True)
+    description = models.CharField(blank=True)
+    image = models.ImageField(upload_to='images/',
+                              default='../default_profile_vvucyn')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Meta:
+    ordering = ['created_at']  
+
+
+"""
+Returns information on who the profile owner is
+"""
+
+
+def __str__(self):
+    return f"{self.owner}'s profile"
