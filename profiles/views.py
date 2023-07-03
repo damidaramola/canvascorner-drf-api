e26c343ad7034e8674c85222e33ad58e9838fe62
damidaramola <damidaramola7@gmail.com>
2023-07-03 22:27:42 +0000

    add is_owner field

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
+from canvascorner_drf_api.permissions import IsOwnerOrReadOnly
 
 
 """
@@ -16,7 +16,9 @@ ProfileList will list all profiles
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(profiles, many=True, context={
+            'request': request
+        })
         return Response(serializer.data)
 
 
@@ -40,7 +42,8 @@ class ProfileDetail(APIView):
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={
+            'request': request})
         return Response(serializer.data)
 
     """
@@ -49,7 +52,8 @@ class ProfileDetail(APIView):
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data, context={
+            'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
