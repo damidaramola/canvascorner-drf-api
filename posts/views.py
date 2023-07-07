8f37cc175ff955891e80bd57fc79def3690e2612
damidaramola <damidaramola7@gmail.com>
2023-07-07 20:32:25 +0000

    bug fix: remove data request in postserializer

--- posts/views.py
+++ posts/views.py
@@ -55,8 +55,12 @@ class PostDetail(APIView):
     def get(self, request, pk):
         post = self.get_object(pk)
         serializer = PostSerializer(
-            post, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            post, context={'request': request})
+        return Response(serializer.data)
+
+    # def get(self, request, pk):
+    #     post = self.get_object(pk)
+    #     serializer = PostSerializer(
+    #         post, data=request.data, context={'request': request})
+    #     return Response(serializer.data)
+   
