70d976bf1cc2fcae9aa15aa3a4703f90e01b77a6
damidaramola <damidaramola7@gmail.com>
2023-07-08 21:39:54 +0000

    bug fix: fix spelling errors in views.py

--- likes/views.py
+++ likes/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics, permissions
 from canvascorner_drf_api.permissions import IsOwnerOrReadOnly
 from likes.models import Likes
-from like.serializers import LikesSerializer
+from likes.serializers import LikesSerializer
 
 
-class LikesList(generic.ListCreateAPIView):
+class LikesList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikesSerializer
     queryset = Likes.objects.all()
