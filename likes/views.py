a4badc01f05810c79752e154595408e752c434a4
damidaramola <damidaramola7@gmail.com>
2023-07-08 21:48:28 +0000

    add urls to likes app

--- likes/views.py
+++ likes/views.py
@@ -8,6 +8,17 @@ class LikesList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikesSerializer
     queryset = Likes.objects.all()
-    
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    """
+    A class for LikeDetail
+    User to be able to retrieve and delete their like
+    """
+
+
+class LikesDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikesDetailSerializer
+    queryset = Likes.objects.all()
