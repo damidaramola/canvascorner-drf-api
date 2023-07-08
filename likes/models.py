a28bf9a8b9e163bb2c44d58d2ffe96d8b73eeffc
damidaramola <damidaramola7@gmail.com>
2023-07-08 20:43:20 +0000

    migrate likes model to database

--- likes/models.py
+++ likes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.model import User
+from django.contrib.auth.models import User
 from posts.models import Post
 
 """ 
@@ -11,11 +11,10 @@ class Likes(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name='likes',
                              on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(add_auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = [-created_at]
+        ordering = ['-created_at']
         unique_together = ['owner', 'post']
         
         def __str__(self):
