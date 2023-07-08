6f2d44c9bc11a9bcd28f54d3db79b92be66e7085
damidaramola <damidaramola7@gmail.com>
2023-07-08 18:33:41 +0000

    create commentslist view

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from canvascorner_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+""" 
+use generic views to handle get and post http requests
+"""
+
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
