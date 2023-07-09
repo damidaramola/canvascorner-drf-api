8098a80fcb8a085d7575da942f62964920abb83f
damidaramola <damidaramola7@gmail.com>
2023-07-09 17:17:35 +0000

    create followerlist and followerdetail views

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from canvascorner_drf_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    """
+    A class for FollowerDetail
+    User to be able to retrieve and delete their follow
+    """
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
