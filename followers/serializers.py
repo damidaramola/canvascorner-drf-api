83c5e9dbfab8762c3e85a4fe8ddeb7f0ad347469
damidaramola <damidaramola7@gmail.com>
2023-07-09 17:07:20 +0000

    create serializer follower class

---
+++ followers/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Follower
+from django.db import IntegrityError
+
+"""Create Follower Serializer
+"""
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Follower
+        fields = ['id',
+                  'owner',
+                  'created_at',
+                  'followed',
+                  'followed_name',]
+
+    """
+    Handle if likes are duplication/integrity errors
+    """
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplication of follow'
+            })
