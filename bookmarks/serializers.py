b8aa85280ec17000f9c2dbd18b7a5c61e9127b77
damidaramola <damidaramola7@gmail.com>
2023-07-09 19:47:51 +0000

    create serializer for bookmark

---
+++ bookmarks/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Bookmark
+from django.db import IntegrityError
+
+"""
+Create Bookmark Serializer
+"""
+
+
+class BookmarkSerializer(serializers.ModelsSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Bookmark
+        fields = ['id',
+                  'owner',
+                  'bookmarked_at',
+                  'post',]
+
+    """
+    Handle if bookmark is duplicated by the same user 
+    """
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplication of bookmark'
+            })
