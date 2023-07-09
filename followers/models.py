e451724095d1dcf57209449b2a9763335c63578f
damidaramola <damidaramola7@gmail.com>
2023-07-09 16:52:30 +0000

    create serializers.py and urls.py in followers folder

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+""" 
+Create follower model which has a foreignkey relationship with 
+User model
+ the 'owner' is a User that is following another User
+ the related_name attribute differentiates between the owner and 
+ followed which are both instances of the user model
+"""
+
+
+class Follower(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='followed')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
