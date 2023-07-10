9363033def5b353ea486b6f579690463f73a322c
damidaramola <damidaramola7@gmail.com>
2023-07-10 21:26:49 +0000

    add bookmark id to posts serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from posts.models import Post
 from likes.models import Likes
+from bookmarks.models import Bookmark
 
 """
 create serializer for posts
@@ -14,6 +15,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    bookmark_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -37,7 +39,7 @@ class PostSerializer(serializers.ModelSerializer):
     """
     check if the user is authenticated
     filter so that logged in user is the user
-    who liked the post
+    who liked/bookmarked the post
     """
 
     def get_like_id(self, obj):
@@ -48,10 +50,19 @@ class PostSerializer(serializers.ModelSerializer):
             ).first()
             return like.id if like else None
         return None
+    
+    def get_bookmark_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            bookmark = Bookmark.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return bookmark.id if bookmark else None
+        return None
         
     class Meta:
         model = Post
         fields = ['id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-                  'created_at', 'updated_at', 'like_id',
+                  'created_at', 'updated_at', 'like_id', 'bookmark_id',
                   'title', 'description', 'category', 'image',
                   ]
