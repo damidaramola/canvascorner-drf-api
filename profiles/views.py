f54719272b675a1de87bb6f1b1ee2fdd7b4bdb6c
damidaramola <damidaramola7@gmail.com>
2023-07-11 21:23:08 +0000

    add filterset firlds to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
 from canvascorner_drf_api.permissions import IsOwnerOrReadOnly
@@ -21,9 +22,14 @@ class ProfileList(generics.ListAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
     ]
-
+    
+    filterset_fields =[
+        'owner__following__followed_profile'
+    ]
+    
     ordering_fields = [
         'posts_count',
         'followers_count',
