422c6bf5f1932fbb9c9cb1f74c6a69cfa544c97d
damidaramola <damidaramola7@gmail.com>
2023-07-11 23:14:12 +0000

    set pagination, default renderer to JSON and date time format

--- canvascorner_drf_api/settings.py
+++ canvascorner_drf_api/settings.py
@@ -30,8 +30,18 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
+
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -69,20 +79,20 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
-    
+
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-    
+
     'profiles',
     'posts',
     'comments',
     'likes',
     'followers',
     'bookmarks',
-    
+
 ]
 
 SITE_ID = 1
