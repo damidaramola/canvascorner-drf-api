5a412cb97e1eb766bfe509203130af903f08810c
damidaramola <damidaramola7@gmail.com>
2023-07-11 22:36:26 +0000

    install django rest auth library

--- canvascorner_drf_api/settings.py
+++ canvascorner_drf_api/settings.py
@@ -50,13 +50,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'profiles',
     'posts',
     'comments',
     'likes',
     'followers',
     'bookmarks',
-    'django_filters',
+    
 ]
 
 MIDDLEWARE = [
